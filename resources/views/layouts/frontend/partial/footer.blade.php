e3b94d8a22211ea507737cbe09aea3d6c532c569
sakibmd <sakibmd.cse@gmail.com>
2020-09-22 03:03:13 +0600

    starting frontend designing

---
+++ resources/views/layouts/frontend/partial/footer.blade.php
@@ -0,0 +1,36 @@
+<footer>
+    <section id="footer">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <h2 class="head">About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore etiqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+          </div>
+          <div class="col-md-3">
+            <h2 class="head">We Provide</h2>
+            <p><i class="fas fa-check"> </i> 100% Satisfaction</p>
+             <p><i class="fas fa-check"> </i> 24/7 Support</p>
+             <p><i class="fas fa-check"> </i> Highly Reliable</p>
+          </div>
+
+          <div class="col-md-3">
+            <h2 class="head">Subscribe</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elitvoluptate consequatur illo eaqueos vero. Facilis vero blanditiis eius.
+            </p>
+          
+          </div>
+          <div class="col-md-3">
+            <h2 class="head">Our Location</h2>
+            <p> <i class="fas fa-map-marker-alt"></i> ABC Limited, Uttara Dhaka</p>
+            <p><i class="fas fa-phone"></i> +88 01612345678</p>
+            <p><i class="fas fa-envelope"></i> jannathsyeda@gmail.com</p>
+          </div>
+        </div>
+        <hr style="background-color: white;" class="pb-0">
+        <p class="text-center p-3 mb-0"> &copy;Newsportal<b></b></p>
+      </div>
+    </section>
+</footer>
---
+++ resources/views/layouts/frontend/partial/header.blade.php
@@ -0,0 +1,78 @@
+<header>
+
+    <style>
+      .navbar ul li a{
+          font-size: 18px;
+          color: white! important;
+          font-weight: bold;
+        }
+        .navbar ul li a:hover{
+          background-color: rgb(203, 206, 205);
+          color: black;
+          padding: 5px;
+          height: 100%;
+        }
+    </style>
+    
+  
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div>
+                        {{-- <img src="{{ asset('backend/img/real-estate-estate-agent-property-management-house-realstate.jpg') }}" style="height: 80px; width: 80px" alt="" class="my-2 text-center"> --}}
+                        <strong class="name">House Rental</strong>
+                        <div class="mb-3" id="time"></div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            {{-- <a class="navbar-brand" href="{{ route('welcome') }}"></a> --}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                  <a class="nav-link" href="{{ route('welcome') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="">All Houses</a>
+                </li>
+               
+
+                @guest
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                @else
+                    @if (Auth::user()->role_id == 2)
+                        <li class="nav-item"><a class="nav-link" href="{{ route('landlord.dashboard') }}">Dashboard</a></li>
+                    @endif
+                    @if (Auth::user()->role_id == 3)
+                        <li class="nav-item"><a class="nav-link" href="{{ route('renter.dashboard') }}">Dashboard</a></li>
+                    @endif
+                    @if (Auth::user()->role_id == 1)
+                        <li class="nav-item"><a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                    @endif
+                @endguest
+              </ul>
+            </div>
+          </nav>
+
+    
+</header>
+
+<script type="text/javascript">
+  
+  var date = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var months = ["January","February","March","April","May","June","July","August","September", "October", "November", "December"];
+  document.getElementById("time").innerHTML = '<strong>' + days[date.getDay()] + '</strong>' + ', ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+  
+</script>
